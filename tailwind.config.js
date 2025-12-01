b092894d500a304f20a395614b3858e307cd5b6e
valdonolvak <159708895+valdonolvak@users.noreply.github.com>
2025-12-01 22:46:30 +0200

    Add TypeScript support to Tailwind config

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,6 @@
-export default {
-  content: ["./index.html", "./src/**/*.{js,jsx}"],
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./index.html", "./src/**/*.{js,jsx,ts,tsx}"],
   theme: {
     extend: {},
   },
